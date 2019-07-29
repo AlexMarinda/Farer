de9d9dc0765b71098bae2dc48bb188c4ee1156aa
AlexMarinda <malalex44@gmail.com>
2019-07-29 19:55:13 +0200

    Ft signup 167294306 (#14)
    * add about us page
    
    * some changes
    
    * Specify a seat number when making a booking page
    
    * SETUP
    to add node file
    to testing file
    adding server folder
    
    * Siginup
    to user registration
    to add validation

---
+++ server/controllers/user.js
@@ -0,0 +1,45 @@
+import {generateToken,encryptPass, checkPassword} from './../helpers';
+import users from '../model/users'
+
+
+//class contain all user operation
+class UserController {
+    
+// new user 
+static  registerUser(req, res) {
+
+const newUser = {
+user_id:users.length + 1,
+email:req.body.email,
+password:encryptPass(req.body.password),
+first_name: req.body.first_name,
+last_name: req.body.last_name
+};
+for (let i =0; i<users.length;i++){
+  
+    if(users[i].email===req.body.email )
+       
+      return res.status(200).send({ status: 'success', data: "choose another email this was taken" });
+      
+           
+        }
+const token = generateToken(users.email);
+users.push(newUser);
+
+return res.status(201).send({ status: 'success', data: { 
+token,
+email:req.body.email,
+password:encryptPass(req.body.password),
+first_name: req.body.first_name,
+last_name: req.body.last_name
+ } });
+
+}
+
+
+
+
+}
+
+
+export default UserController;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import routes from './routes';
+
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+  const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const port = process.env.PORT || 5000;
+
+
+
+app.use(routes);
 
 
   const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
+
 
 
 app.use('*', (req, res) => res.status(404).send({
