7cfbb9c071359882ccefd662e2f7022fb6a1bd4e
AlexMarinda <malalex44@gmail.com>
2019-07-29 19:50:23 +0200

    Siginup to user registration to add validation

---
+++ server/routes/api/user.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import UserController from '../../controllers/user';
+import {Validation} from '../../middleware/validation';
+
+
+
+
+const router = express.Router();
+
+router.post('/signup',Validation.userValidator, UserController.registerUser);
+
+
+export default router;
