e9a4033b271debf82ebf68947bf4ddf81448941f
AlexMarinda <malalex44@gmail.com>
2019-07-30 10:34:53 +0200

    CREATE TRIP admin can create atrip validation information needed check if user is admin

---
+++ server/routes/api/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import user from './user';
+import trip from './trip';
+
+
+
+const router = express.Router();
+
+router.use('/auth', user);
+router.use('/trips', trip);
+
+
+
+export default router;
