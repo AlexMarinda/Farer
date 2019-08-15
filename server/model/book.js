787b16e4fda84e1dcd8557920a1bbfaa2c40c8ff
AlexMarinda <malalex44@gmail.com>
2019-08-15 23:54:13 +0200

    [finishes #167929567] add tests  (#62)
    * add about us page
    
    * some changes
    
    * Specify a seat number when making a booking page
    
    * SETUP
    to add node file
    to testing file
    adding server folder
    
    * some changes
    
    * make better
    
    * make better
    
    * some changes
    
    * to add some changes
    
    * to add some changes
    
    * to add some changes
    
    * to add some changes
    
    * to add some changes
    
    * some change
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes'
    '
    
    * some changes
    
    * some changes
    
    * some node modules changes
    
    * some change
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some change
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some chabges
    
    * some chabges
    
    * some chabges
    
    * some changes
    
    * END POINTS CHECK
    to collect endpoint
    
    * some changes
    
    * some change
    
    * some change
    
    * some change
    
    * some change
    
    * some change
    
    * some change
    
    * some changes
    
    * some cheanges
    
    * some changes
    
    * testing

--- server/model/book.js
+++ server/model/book.js
@@ -8,6 +8,22 @@ const bookings = [
     user_id: 1,
   },
 
+  {
+    book_id: 2,
+    seat_number: 2,
+    created_on: '2019-08-07T17:39:17+02:00',
+    trip_id: 3,
+    user_id: 1,
+  },
+
+  {
+    book_id: 3,
+    seat_number: 2,
+    created_on: '2019-08-07T17:39:17+02:00',
+    trip_id: 2,
+    user_id: 1,
+  },
+
 ];
 
 export default bookings;
--- server/test/book.test.js
+++ server/test/book.test.js
@@ -1,47 +1,51 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../index';
+import { initDB } from '../way_fareDB/dbInit';
 
 
 chai.use(chaiHttp);
 
 chai.should()
 
+describe('ALL booking /', () => {
 
-
-describe('GET /', () => {
   it('it should display all bookings', done => {
     chai
       .request(app)
       .get('/api/v1/bookings')
       .set('content-type', 'application/json')
-      .set('Authorization', `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfaWQiOiI4ZjZhNTNjYi1jY2VlLTQzYjYtYjQzMSIsImVtYWlsIjoibWFsYWxleDQ0QGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiJDJiJDEwJC9ZMHkvOTA1cGNSc1V6N0oxT2U2OC5LZjQ4UVJYcmxRNjh4aGNPWnp5cExpa0h0ZTJCV1Z5IiwiZmlyc3RfbmFtZSI6ImFsZXgiLCJsYXN0X25hbWUiOiJtYXJpbmRhIiwiaXNfYWRtaW4iOnRydWV9LCJpYXQiOjE1NjUyMDk3MTR9.NgQtSDvMOmyMEhx5ybc6t_ZK3u4nO6TJEouigrFZzJo"}`)
+      .set('Authorization', `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6Im1hbGFsZXg0NEBnbWFpbC5jb20iLCJmaXJzdF9uYW1lIjoiYWxleCIsImxhc3RfbmFtZSI6Im1hcmluZGEiLCJwYXNzd29yZCI6IiQyYiQxMCQvWTB5LzkwNXBjUnNVejdKMU9lNjguS2Y0OFFSWHJsUTY4eGhjT1p6eXBMaWtIdGUyQldWeSIsImlzX2FkbWluIjp0cnVlLCJpYXQiOjE1NjU4MTEyMDB9.ardfycpDYYyGlGmlAFqkcVVr-AfQQx2qvIqxz_74h7w"}`)
       .end((err, res) => {
-        
         res.should.have.status(200);
-        res.body.should.have.property('data');
         done();
       });
   });
 
 
-    });
-
-    /*describe('DELETE/', () => {
-      it('it should delete booking ', done => {
-             chai.request(app)
-             .delete('/api/v1/bookings/1/delete/')
-             .set('content-type', 'application/json')
-             .set('Authorization', `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfaWQiOiI4ZjZhNTNjYi1jY2VlLTQzYjYtYjQzMSIsImVtYWlsIjoibWFsYWxleDQ0QGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiJDJiJDEwJC9ZMHkvOTA1cGNSc1V6N0oxT2U2OC5LZjQ4UVJYcmxRNjh4aGNPWnp5cExpa0h0ZTJCV1Z5IiwiZmlyc3RfbmFtZSI6ImFsZXgiLCJsYXN0X25hbWUiOiJtYXJpbmRhIiwiaXNfYWRtaW4iOnRydWV9LCJpYXQiOjE1NjUyMDk3MTR9.NgQtSDvMOmyMEhx5ybc6t_ZK3u4nO6TJEouigrFZzJo"}`)
    
-             .end((err, res) => {
-               res.should.have.status(200);
-               res.body.should.have.property('status').eql(200);
-               res.body.should.have.property('message').eql("Booking deleted successfully");
-            
-               done()
-             })
-         });
+        it('it should booking a seat', done => {
+          chai
+            .request(app)
+            .post('/api/v1/bookings')
+            .set('content-type', 'application/json')
+            .set('Authorization', `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfaWQiOiI4ZjZhNTNjYi1jY2VlLTQzYjYtYjQzMSIsImVtYWlsIjoibWFsYWxleDQ0QGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiJDJiJDEwJC9ZMHkvOTA1cGNSc1V6N0oxT2U2OC5LZjQ4UVJYcmxRNjh4aGNPWnp5cExpa0h0ZTJCV1Z5IiwiZmlyc3RfbmFtZSI6ImFsZXgiLCJsYXN0X25hbWUiOiJtYXJpbmRhIiwiaXNfYWRtaW4iOnRydWV9LCJpYXQiOjE1NjUyMDk3MTR9.NgQtSDvMOmyMEhx5ybc6t_ZK3u4nO6TJEouigrFZzJo"}`)
+            .send({
+                "trip_id":500
+
+              })
+            .end((err, res) => {
+              res.should.have.status(409);
+              done();
+            });
+        });
+
+
+
+
+
+
      
-       })*/
-     
+      
+          });
+
